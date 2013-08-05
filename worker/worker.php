8842c4e724ab861080a4906b9451b8b40a8170ce
Anton Anisimov <masterpersi@gmail.com>
2013-08-06 01:02:50 +0600

    worker errors

--- worker/worker.php
+++ worker/worker.php
@@ -234,6 +234,7 @@ function convertJob(GearmanJob $job)
         }, $convert_errors);
 
         if (!$r) {
+            $convert_errors = $convert_errors['error'] . ': ' . implode(', ', $convert_errors);
             throw new Exception(implode(', ', $convert_errors));
         }
 
@@ -251,7 +252,7 @@ function convertJob(GearmanJob $job)
             $sth->execute(['files_urls' => json_encode($files_urls), 'row_id' => $payload['row_id']]);
 
             if (!$r) {
-                $convert_errors = implode(', ', $convert_errors);
+                $convert_errors = $convert_errors['error'] . ': ' . implode(', ', $convert_errors);
                 throw new Exception($convert_errors);
             }
         }
